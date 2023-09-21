d3c278c050416bca01ec8c032edcb19d2ed54f6f
Shaun Omari <Shaunomari72@gmail.com>
2023-09-21 21:53:10 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    return bestCustomer = newBestCustomer
+}
+
+let leastFavoriteCharacter = "John"
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCharacter
+}
